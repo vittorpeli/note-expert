3e102653c5d0733120fbb99d5d98621cfd53fa1e
vittor <vittor.pelicarto137@gmail.com>
2024-02-06 19:37:57 -0300

    Add Modal

--- src/components/note-card.tsx
+++ src/components/note-card.tsx
@@ -29,7 +29,7 @@ export function NoteCard ({note}: NoteCardProps) {
 
         </Dialog.Overlay>
         <Dialog.Content className="fixed overflow-hidden left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2 max-w-[640px] w-full h-[60vh] bg-slate-700 rounded-md flex flex-col outline-none">
-          <Dialog.Close className="absolute right-0 top-0 bg-slate-800 p-1.5 text-slate-400 hover:text-slate-200">
+          <Dialog.Close className="absolute right-0 top-0 outline-none bg-slate-800 p-1.5 text-slate-400 hover:text-slate-200">
             <X className="size-5"/>
           </Dialog.Close>
 
