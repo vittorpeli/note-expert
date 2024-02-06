2305827af7e25d01949afc76aa19c3a853cf8741
vittor <vittor.pelicarto137@gmail.com>
2024-02-06 18:42:28 -0300

    first commit

---
+++ src/app.tsx
@@ -0,0 +1,29 @@
+import logo from './assets/Logo.svg'
+import { NewNoteCard } from './components/new-note-card'
+import { NoteCard } from './components/note-card'
+
+export default function App() {
+  return (
+    <div className='mx-auto max-w-6xl my-12 py-0 px-5 space-y-6'>
+      <img src={logo} alt="NLW Expert" />
+
+      <form className='w-full'>
+        <input 
+          type="text" 
+          placeholder='Busque em suas notas...'
+          className='w-full bg-transparent text-3xl font-semibold tracking-tight placeholder:text-slate-500 outline-none'
+        />
+      </form>
+
+      <div className='h-px bg-slate-700'/>
+
+      <div className="grid grid-cols-3 gap-4 auto-rows-[250px]">
+        
+        <NewNoteCard />
+
+        <NoteCard date={new Date()} content='Hello World'/>
+        
+      </div>
+    </div>
+  )
+}
---
+++ src/components/note-card.tsx
@@ -0,0 +1,18 @@
+interface NoteCardProps {
+  date: Date
+  content: string
+}
+
+export function NoteCard (props: NoteCardProps) {
+  return (
+    <button className='rounded-md text-left flex flex-col bg-slate-800 p-5 gap-3 overflow-hidden relative outline-none hover:ring-2 hover:ring-slate-600 focus-visible:ring-1 focus-visible:ring-lime-400'>
+      <span className='text-sm font-medium text-slate-200'>
+          {props.date.toISOString()}
+        </span>
+        <p className='text-sm leading-6 text-slate-400'>
+          {props.content}
+        </p>
+        <div className="absolute bottom-0 left-0 right-0 h-1/2 bg-gradient-to-t from-black/60 to-black/0 pointer-events-none"/>
+    </button>
+  )
+}
